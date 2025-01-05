554bf3e0acc33cc2417844d4901daa0cd4a6e025
Humberto Alves <hjalves@live.com>
2025-01-05 15:48:03 +0000

    Implement scrolling

--- xayos/voyager.py
+++ xayos/voyager.py
@@ -5,7 +5,7 @@ import sdl2.ext
 from sdl2 import sdlgfx
 
 from . import colors
-from .gamepad import BUTTON_START
+from .gamepad import BUTTON_START, BUTTON_DPAD_DOWN, BUTTON_DPAD_UP
 from .input import MenuController
 from .menu import Menu
 
@@ -38,7 +38,6 @@ class Voyager:
         self.text_viewer = TextViewer(
             self.font_loader,
             font_name="10x20",
-            text="Gemini Voyager\n\nPress START to open the menu.",
         )
 
     def update(self, elapsed_ms):
@@ -68,16 +67,21 @@ class Voyager:
             self.toggle_menu()
         if self.menu.active:
             self.menu_controller.handle_input(button, state)
+        else:
+            if button == BUTTON_DPAD_DOWN and state:
+                self.text_viewer.scroll_down()
+            elif button == BUTTON_DPAD_UP and state:
+                self.text_viewer.scroll_up()
 
     def open_location(self):
         log.info("Open Location...")
         # Fetch capsule content
-        # response = ignition.request('//geminiprotocol.net/')
+        response = ignition.request("//geminiprotocol.net/docs/faq.gmi")
         # Get status from remote capsule
         # print(response.status)
         # Get response information from remote capsule
-        # data = response.data()
-        data = "# Project Gemini\n\n## Gemini in 100 words\n\nGemini is a new internet technology supporting an electronic library of interconnected text documents.  That's not a new idea, but it's not old fashioned either.  It's timeless, and deserves tools which treat it as a first class concept, not a vestigial corner case.  Gemini isn't about innovation or disruption, it's about providing some respite for those who feel the internet has been disrupted enough already.  We're not out to change the world or destroy other technologies.  We are out to build a lightweight online space where documents are just documents, in the interests of every reader's privacy, attention and bandwidth.\n\n=> docs/faq.gmi\tIf you'd like to know more, read our FAQ\n=> https://www.youtube.com/watch?v=DoEI6VzybDk\tOr, if you'd prefer, here's a video overview\n\n## Official resources\n\n=> news/\tProject Gemini news\n=> docs/\tProject Gemini documentation\n=> history/\tProject Gemini history\n=> software/\tKnown Gemini software\n\nAll content at geminiprotocol.net is CC BY-NC-ND 4.0 licensed unless stated otherwise:\n=> https://creativecommons.org/licenses/by-nc-nd/4.0/\tCC Attribution-NonCommercial-NoDerivs 4.0 International\n"
+        data = response.data()
+        # data = "# Project Gemini\n\n## Gemini in 100 words\n\nGemini is a new internet technology supporting an electronic library of interconnected text documents.  That's not a new idea, but it's not old fashioned either.  It's timeless, and deserves tools which treat it as a first class concept, not a vestigial corner case.  Gemini isn't about innovation or disruption, it's about providing some respite for those who feel the internet has been disrupted enough already.  We're not out to change the world or destroy other technologies.  We are out to build a lightweight online space where documents are just documents, in the interests of every reader's privacy, attention and bandwidth.\n\n=> docs/faq.gmi\tIf you'd like to know more, read our FAQ\n=> https://www.youtube.com/watch?v=DoEI6VzybDk\tOr, if you'd prefer, here's a video overview\n\n## Official resources\n\n=> news/\tProject Gemini news\n=> docs/\tProject Gemini documentation\n=> history/\tProject Gemini history\n=> software/\tKnown Gemini software\n\nAll content at geminiprotocol.net is CC BY-NC-ND 4.0 licensed unless stated otherwise:\n=> https://creativecommons.org/licenses/by-nc-nd/4.0/\tCC Attribution-NonCommercial-NoDerivs 4.0 International\n"
         self.text_viewer.set_text(data)
 
 
@@ -86,9 +90,9 @@ class TextViewer:
         self,
         font_loader,
         width=940,
-        height=520,
+        height=500,
         font_name="10x20",
-        text="<Insert text here>",
+        text="",
         fg=colors.LIGHT_GREY_2,
         line_spacing=0,
     ):
@@ -104,83 +108,47 @@ class TextViewer:
         self.fg = fg
         self.line_spacing = line_spacing
         self.font_loader.set_font(self.font)
-        self.cursor_cx = 0
-        self.cursor_cy = 0
-        self.cursor_char = None
-        self.cursor_color = colors.PINK
-        self.cursor_char_color = colors.PINK
-        self.update_cursor_position()
+        self.line_offset = 0
+        self.lines = []
+        self.width_chars = 0
+        self.height_chars = 0
+        self.update_lines()
 
     def get_text(self):
         return self.text
 
     def set_text(self, text):
         self.text = text
-        self.update_cursor_position()
+        self.line_offset = 0
+        self.update_lines()
 
-    def put_char(self, char):
-        # TODO: put under cursor
-        self.text += char
-        self.update_cursor_position()
+    def scroll_up(self):
+        self.line_offset -= 1
+        if self.line_offset < 0:
+            self.line_offset = 0
+        log.debug(f"Scrolling up to {self.line_offset}")
 
-    def backspace(self):
-        # TODO: remove char before cursor
-        self.text = self.text[:-1]
-        self.update_cursor_position()
+    def scroll_down(self):
+        self.line_offset += 1
+        max_offset = max(len(self.lines) - self.height_chars, 0)
+        if self.line_offset > max_offset:
+            self.line_offset = max_offset
+        log.debug(f"Scrolling down to {self.line_offset}")
 
-    def delete(self):
-        # TODO: for now delete the line
-        lines = self.text.split("\n")
-        self.text = "\n".join(lines[:-1])
-        self.update_cursor_position()
-
-    def clear(self):
-        self.text = ""
-        self.update_cursor_position()
-
-    def update_cursor_position(self):
-        lines = self.text.split("\n")
-        self.cursor_cx = len(lines[-1])
-        self.cursor_cy = len(lines) - 1
-
-    def set_cursor_char(self, char):
-        assert char is None or isinstance(char, bytes) and len(char) == 1
-        self.cursor_char = char
-
-    def set_cursor_color(self, color):
-        self.cursor_color = color
-
-    def render_cursor(self, sdlrenderer):
+    def update_lines(self):
+        # Calculate width and height in characters
         font_size = self.font_loader.get_font_size(self.font)
-        cursor_x = 0 + self.cursor_cx * font_size[0]
-        cursor_y = 0 + self.cursor_cy * font_size[1]
-        sdlgfx.boxRGBA(
-            sdlrenderer,
-            cursor_x,
-            cursor_y + font_size[1] - 2,
-            cursor_x + font_size[0],
-            cursor_y + font_size[1],
-            *self.cursor_color,
-        )
-        if self.cursor_char:
-            self.font_loader.set_font(self.font)
-            sdlgfx.stringRGBA(
-                sdlrenderer, cursor_x, cursor_y, self.cursor_char, *self.cursor_char_color
-            )
+        self.width_chars = self.width // font_size[0]
+        self.height_chars = self.height // font_size[1]
+        self.lines = self.split_text_into_lines(self.text, self.width_chars)
 
     def draw_to_surface(self):
-        # self.renderer.clear((0x30, 0x30, 0x40, 0x80))
         self.renderer.clear(colors.TRANSPARENT)
 
         self.font_loader.set_font(self.font)
-        # Calculate width and height in characters
         font_size = self.font_loader.get_font_size(self.font)
-        width_chars = self.width // font_size[0]
-        height_chars = self.height // font_size[1]
 
-        lines = self.split_text_into_lines(self.text, width_chars, height_chars)
-
-        # Split text into lines
+        lines = self.lines[self.line_offset : self.line_offset + self.height_chars]
         for i, line in enumerate(lines):
             y = i * font_size[1] + i * self.line_spacing
             sdlgfx.stringRGBA(
@@ -194,9 +162,8 @@ class TextViewer:
                 self.fg[3],
             )
 
-    def split_text_into_lines(self, text, width_chars, height_chars):
+    def split_text_into_lines(self, text, width_chars):
         lines = []
-
         for line in wrap_text(text, width_chars):
             line = line.encode()
             line = line.replace(b"\t", b" ")  # Replace tabs with 1 space for now
@@ -210,7 +177,6 @@ class TextViewer:
                 else:
                     lines.append(line[:width_chars])
                     line = line[width_chars:]
-
         return lines
 
     def render(self, renderer, x=0, y=0):
